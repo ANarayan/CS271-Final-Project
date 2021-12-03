b1b9e7fe240998b64393ac006dcbbc348fe72a12
ANarayan <gostanfordtennis@gmail.com>
2021-12-03 14:28:37 -0800

    [add] mlp encoder

--- encoder_layers.py
+++ encoder_layers.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 from keras.layers import LSTM
+import tensorflow_addons as tfa
 
 
 class TransformerBlock(layers.Layer):
@@ -31,6 +32,23 @@ class TransformerBlock(layers.Layer):
         return self.layernorm2(out1 + ffn_output)
 
 
+class MLP(layers.Layer):
+    def __init__(self, max_len, d_model, dropout_rate, *args, **kwargs):
+        super(MLP, self).__init__(*args, **kwargs)
+
+        self.mlp = keras.Sequential(
+            [
+                layers.Dense(units=max_len),
+                tfa.layers.GELU(),
+                layers.Dense(units=d_model),
+                layers.Dropout(rate=dropout_rate),
+            ]
+        )
+
+    def call(self, inputs):
+        return self.mlp(inputs)
+
+
 def LSTMEncoder(d_model, **kwargs):
     return LSTM(units=d_model, **kwargs)
 
@@ -38,6 +56,10 @@ def LSTMEncoder(d_model, **kwargs):
 def TransformerEncoder(d_model, num_heads, ff_dim, rate, **kwargs):
     transformer = TransformerBlock(d_model, num_heads, ff_dim, rate)
     return transformer
+
+
+def MLPEncoder(max_len, d_model, dropout_rate, *args, **kwargs):
+    return MLP(max_len, d_model, dropout_rate, *args, **kwargs)
 
 
 ENCODER_REGISTRY = {"lstm": LSTMEncoder, "transformer": TransformerEncoder}
